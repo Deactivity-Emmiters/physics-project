c9e57f7683cb4dc8390062feecf27e18f09d3459
NanzeRT <nanotest.vlad@gmail.com>
2024-05-01 05:58:00 +0300

    Added camera controls and diagnostics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::input::mouse::MouseMotion;
 use bevy::prelude::*;
+use bevy::window::{CursorGrabMode, PrimaryWindow};
 
 #[derive(Component)]
 struct Electron;
@@ -9,20 +12,38 @@ struct Velocity(Vec3);
 #[derive(Resource)]
 struct SpawnTimer(Timer);
 
+#[derive(Component)]
+struct CameraAngles {
+    vertical: Quat,
+    horizontal: Quat,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FrameTimeDiagnosticsPlugin)
+        .add_plugins(LogDiagnosticsPlugin::default())
         .insert_resource(SpawnTimer(Timer::from_seconds(0.3, TimerMode::Repeating)))
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (spawn_electrons, move_by_velocity, apply_gravity))
+        .add_systems(
+            FixedUpdate,
+            (spawn_electrons, move_by_velocity, apply_gravity),
+        )
+        .add_systems(Update, camera_controls)
         .run();
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
-        ..Default::default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
+            ..Default::default()
+        },
+        CameraAngles {
+            vertical: Quat::from_rotation_x(0.0),
+            horizontal: Quat::from_rotation_y(0.0),
+        },
+    ));
 }
 
 fn spawn_electrons(
@@ -59,10 +80,24 @@ fn move_by_velocity(time: Res<Time>, mut query: Query<(&Velocity, &mut Transform
 }
 
 /// Placeholder
-fn apply_gravity(
-    time: Res<Time>,
-    mut query: Query<&mut Velocity>) {
+fn apply_gravity(time: Res<Time>, mut query: Query<&mut Velocity>) {
     for mut velocity in query.iter_mut() {
         velocity.0.y -= 9.81 * time.delta_seconds();
     }
+}
+
+fn camera_controls(
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    buttons: Res<ButtonInput<MouseButton>>,
+    mut query: Query<(&mut Transform, &mut CameraAngles), With<Camera3d>>,
+) {
+    if !buttons.pressed(MouseButton::Left) {
+        return;
+    }
+    let (mut transform, mut angles) = query.single_mut();
+    for event in mouse_motion_events.read() {
+        angles.vertical *= Quat::from_rotation_x(-event.delta.y * 0.002);
+        angles.horizontal *= Quat::from_rotation_y(-event.delta.x * 0.002);
+    }
+    transform.rotation = angles.vertical * angles.horizontal;
 }
